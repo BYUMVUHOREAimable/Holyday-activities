5be573b4dd2b39eae56078948ce2368ccb9087b6
BYUMVUHOREAimable <aimablebyumvuhore@gmail.com>
2023-08-20 12:34:31 +0200

    Make the cart quantity interactive

--- amazon/amazon.js
+++ amazon/amazon.js
@@ -75,6 +75,13 @@ cart.push({
     });
     }
 
-    console.log(cart);
+    let cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+    
   });
 });
